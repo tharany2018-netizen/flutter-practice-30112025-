d22c057e9b91982198ad586c11ae9281cedd3b82
tharany2018-netizen <tharany2018@gmail.com>
2025-12-02 22:23:57 +0530

    class

---
+++ vechicle.dart
@@ -0,0 +1,19 @@
+class Vechicle{
+  final String? type;
+
+final String ? model;
+
+  const Vechicle ({this.type,this.model});
+
+  start(){
+    print ("started");
+  }
+  information(){
+    print ("you are using $type $model");
+
+  }
+  stop(){
+    print("stop");
+
+  }
+  }
